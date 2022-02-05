ec60ccb73165b54791d04a528577f4bea482e922
Cesar Gonzalez Groh <clgonzalezgroh@hotmail.com>
2022-02-05 20:12:08 -0300

    Final curso de Manejo Profesional del Estado

--- src/components/App/useLocalStorage.js
+++ src/components/App/useLocalStorage.js
@@ -1,36 +1,74 @@
-import { useState, useEffect } from "react"
+import { useEffect, useReducer } from "react"
 
 const useLocalStorage = (key, initialValue) => {
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
-  const [synchronizedItem, SetSynchronizedItem] = useState(true)
+  const initialState = {
+    loading: true,
+    error: false,
+    synchronizedItem: true,
+    storageValue: initialValue,
+  }
 
-  const [storageValue, setStorageValue] = useState(initialValue)
+  const actionTypes = {
+    error: "ERROR",
+    success: "SUCCESS",
+    save: "SAVE",
+    synchronize: "SYNCHRONIZE",
+  }
+  const reducerObject = (state, payload) => ({
+    [actionTypes.error]: { ...state, error: true },
+    [actionTypes.success]: {
+      ...state,
+      error: false,
+      loading: false,
+      storageValue: payload,
+      synchronizedItem: true,
+    },
+    [actionTypes.save]: { ...state, storageValue: payload },
+    [actionTypes.synchronize]: {
+      ...state,
+      synchronizedItem: false,
+      loading: true,
+    },
+  })
+  const reducer = (state, action) =>
+    reducerObject(state, action.payload)[action.type] || state
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { loading, error, synchronizedItem, storageValue } = state
+
+  // ACTION CREATORS
+  const onError = (error) =>
+    dispatch({ type: actionTypes.error, payload: error.message })
+  const onSuccess = (parsedItem) =>
+    dispatch({ type: actionTypes.success, payload: parsedItem })
+  const onSave = (newValue) =>
+    dispatch({ type: actionTypes.save, payload: newValue })
+  const onSynchronize = () => dispatch({ type: actionTypes.synchronize })
 
   useEffect(() => {
     setTimeout(() => {
       try {
-        const localStorageItem = JSON.parse(localStorage.getItem(key))
-        if (localStorageItem) {
-          setStorageValue(localStorageItem)
+        const localStorageItem = localStorage.getItem(key)
+        let parsedItem
+        if (!localStorageItem) {
+          localStorage.setItem(key, initialValue)
+          parsedItem = initialValue
+        } else {
+          parsedItem = JSON.parse(localStorageItem)
         }
-        setLoading(false)
-        SetSynchronizedItem(true)
+        onSuccess(parsedItem)
       } catch (error) {
-        setError(error.message)
+        onError(error)
       }
     }, 1000)
   }, [synchronizedItem])
 
   const saveValue = (newValue) => {
-    setStorageValue(newValue)
+    onSave(newValue)
     localStorage.setItem(key, JSON.stringify(newValue))
   }
 
-  const synchronize = () => {
-    setLoading(true)
-    SetSynchronizedItem(false)
-  }
+  const synchronize = onSynchronize
 
   return { storageValue, saveValue, loading, error, synchronize }
 }
